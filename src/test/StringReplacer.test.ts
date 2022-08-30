70f428e46e2bbffca4e3019d67bc8f4e9f88f239
Jayda F <jayda@bokeo.co>
2022-08-31 10:10:31 +1200

    adding deployment to firebade code

--- src/test/StringReplacer.test.ts
+++ src/test/StringReplacer.test.ts
@@ -8,7 +8,7 @@ type StringTestVariables = {
 describe("Replaces special characters ! @ # $ % ^ & * . with a space", () => {
   const stringTestVariables: StringTestVariables[] = [
     { input: "hello%world", expected: "hello world" },
-    { input: "!hello wo*rld", expected: "hello world" },
+    { input: "!hello world", expected: "hello world" },
     { input: ";hello?world!", expected: "hello world" },
     { input: "hello;world", expected: "hello world" },
     { input: "my%name*is!adan#@#$", expected: "my name is adan" },
